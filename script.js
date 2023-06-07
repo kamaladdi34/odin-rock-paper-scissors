191186d3498da04cd502b46ea23de03d72d0b9ab
Kamal Addi <kamal.addi.dev@gmail.com>
2023-06-07 19:40:01 +0100

    Add feature to display 5 rounds winner

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const userButtons = document.querySelectorAll('.user-side .container-row > butto
 const userChoiceEL = document.querySelector('.user-choice');
 const computerChoiceEL = document.querySelector('.computer-choice');
 const scores = document.querySelector('.scores > p')
+const roundInfo = document.querySelector('.round-info');
+const gameWinner = document.querySelector('.game-winner');
 let userScore = 0, computerScore = 0;
 function getComputerChoice() {
   return hands[Math.floor(Math.random() * hands.length)];
@@ -39,12 +41,15 @@ function playRound(userChoice, computerChoice) {
     (userChoice === "scissors" && computerChoice === "paper")
   ) {
     userScore++;
+    roundInfo.textContent = "you won!";
   } else if (userChoice === computerChoice) {
-    return "It's a draw!";
+    roundInfo.textContent = "It's a draw!";
   } else {
     computerScore++;
+    roundInfo.textContent = "beep boop!, robot won!";
   }
   scores.textContent = `${userScore} - ${computerScore}`;
+  gameWinner.textContent = updateWinnerText();
 }
 userButtons.forEach((button)=>{
   button.addEventListener('click',(e)=> {
@@ -68,5 +73,20 @@ function getEmojiFromChoice(choice)
     case 'paper':
       return '📄'
       break;
+  }
+}
+function updateWinnerText()
+{
+  if(userScore==5)
+  {
+    return 'you won the game!, great job! 😀'
+  }
+  else if(computerScore==5)
+  {
+    return 'oh oh!, looks like robot won! 😞'
+  }
+  else
+  {
+    return 'first to 5 wins the game! 😁'
   }
 }
